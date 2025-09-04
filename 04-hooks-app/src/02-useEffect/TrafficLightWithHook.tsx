90af7f37c6b271e72b1447c1146916f18809b54a
Hugo <hugo_mendoza_s@outlook.com>
2025-09-04 17:26:46 -0500

    section hooks app

---
+++ 04-hooks-app/src/02-useEffect/TrafficLightWithHook.tsx
@@ -0,0 +1,25 @@
+import { useTrafficLight } from '../hooks/useTrafficLight';
+
+export const TrafficLightWithHook = () => {
+  const { countdown, greenLight, redLight, yellowLight, percentage } =
+    useTrafficLight();
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
+      <div className="flex flex-col items-center space-y-8">
+        <h1 className="text-white text-2xl">Semáforo con useEffect</h1>
+        <h2 className="text-white text-xl">Countdown: {countdown}</h2>
+
+        <div className="w-64 bg-gray-700 rounded-full h-2">
+          <div
+            style={{ width: `${percentage}%` }}
+            className="bg-blue-500 h-2 rounded-full transition-all duration-1000 ease-linear"
+          ></div>
+        </div>
+        <div className={`w-32 h-32 ${redLight} rounded-full`}></div>
+        <div className={`w-32 h-32 ${yellowLight} rounded-full`}></div>
+        <div className={`w-32 h-32 ${greenLight} rounded-full`}></div>
+      </div>
+    </div>
+  );
+};
