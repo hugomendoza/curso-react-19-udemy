0a2a0155916ed130ed737ed5f1b76eada9456053
Hugo <hugo_mendoza_s@outlook.com>
2025-10-10 17:10:46 -0500

    feat: finish markup teslo shop

--- 07-teslo-shop/teslo-shop/src/shop/pages/gender/GenderPage.tsx
+++ 07-teslo-shop/teslo-shop/src/shop/pages/gender/GenderPage.tsx
@@ -1,3 +1,25 @@
+import { CustomPagination } from '@/components/custom/CustomPagination';
+import { products } from '@/mocks/products.mock';
+import { CustomJumbotron } from '@/shop/components/CustomJumbotron';
+import { ProductsGrid } from '@/shop/components/ProductsGrid';
+import { useParams } from 'react-router';
+
 export const GenderPage = () => {
-  return <div>GenderPage</div>;
+  const { gender = '' } = useParams();
+
+  const genderLabel: Record<string, string> = {
+    men: 'Hombres',
+    women: 'Mujeres',
+    kid: 'Niños',
+  };
+
+  return (
+    <>
+      <CustomJumbotron
+        title={`Productos para ${genderLabel[gender] || 'Todos'}`}
+      />
+      <ProductsGrid products={products} />
+      <CustomPagination totalPages={7} />
+    </>
+  );
 };
--- 07-teslo-shop/teslo-shop/src/shop/pages/home/HomePage.tsx
+++ 07-teslo-shop/teslo-shop/src/shop/pages/home/HomePage.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import { CustomPagination } from '@/components/custom/CustomPagination';
+import { products } from '@/mocks/products.mock';
+import { CustomJumbotron } from '@/shop/components/CustomJumbotron';
+import { ProductsGrid } from '@/shop/components/ProductsGrid';
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <CustomJumbotron title="Todos los productos" />
+      <ProductsGrid products={products} />
+      <CustomPagination totalPages={7} />
+    </>
+  );
 };
