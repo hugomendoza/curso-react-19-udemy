5df5c5100932b5b7ed8940d47146701e0cbf8a2e
Hugo <hugo_mendoza_s@outlook.com>
2025-10-10 18:00:15 -0500

    feat: backend teslo shop

--- 07-teslo-shop/teslo-shop/src/TesloShopApp.tsx
+++ 07-teslo-shop/teslo-shop/src/TesloShopApp.tsx
@@ -1,6 +1,17 @@
 import { RouterProvider } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import { appRouter } from './router/app.router';
 
+const queryClient = new QueryClient();
+
 export const TesloShopApp = () => {
-  return <RouterProvider router={appRouter} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {/* The rest of your application */}
+      <RouterProvider router={appRouter} />;
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
